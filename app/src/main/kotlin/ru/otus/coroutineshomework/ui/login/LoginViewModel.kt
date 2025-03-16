c06667b6100c21fcf7a418b7f9ff4b6fd4116b38
Альберт <rubberbigpepper@gmail.com>
2025-03-16 20:36:55 +0500

    Task 2.1 login coroutine

--- app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
@@ -3,8 +3,13 @@ package ru.otus.coroutineshomework.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import ru.otus.coroutineshomework.ui.login.data.Credentials
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(private val loginApi: LoginApi = LoginApi()) : ViewModel() {
 
     private val _state = MutableLiveData<LoginViewState>(LoginViewState.Login())
     val state: LiveData<LoginViewState> = _state
@@ -15,13 +20,35 @@ class LoginViewModel : ViewModel() {
      * @param password user password
      */
     fun login(name: String, password: String) {
-        // TODO: Implement login
+        viewModelScope.launch {
+            _state.value = LoginViewState.LoggingIn
+            try {
+                val user = withContext(Dispatchers.IO){
+                    loginApi.login(Credentials(name, password))
+                }
+                _state.value =LoginViewState.Content(user)
+            }
+            catch (ex: Exception){
+                _state.value = LoginViewState.Login(error = ex)
+            }
+        }
     }
 
     /**
      * Logout from the network
      */
     fun logout() {
-        // TODO: Implement logout
+        viewModelScope.launch {
+            _state.value = LoginViewState.LoggingOut
+            try {
+                val user = withContext(Dispatchers.IO){
+                    loginApi.logout()
+                }
+                _state.value =LoginViewState.Login()
+            }
+            catch (ex: Exception){
+                _state.value = LoginViewState.Login(error = ex)
+            }
+        }
     }
 }
